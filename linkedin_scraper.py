d7d6dc884b66c780b660ff1619f2a740d2bb55fa
Arif <arahman.isrt@gmail.com>
2024-06-08 16:31:59 -0500

    apply try catch for each variable

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -9,6 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from sleep_helper import SleepHelper
+from web_scraper_helper import WebScraperHelper
 
 # Load environment variables from .env file
 load_dotenv()
@@ -33,11 +34,13 @@ driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 # Connect to MongoDB
 client = MongoClient(MONGO_DB_URL)
 db = client['job_miner']
-collection = db['jobs_linkedin']
+collection = db['jobs_circular']
 
 # Define the base URL and number of pages to scrape
 base_url = 'https://www.linkedin.com/jobs/search/?keywords=data%20engineer&location=California%2C%20United%20States'
 num_pages = 2
+
+SleepHelper.random_sleep(15, 3)
 
 for page in range(num_pages):
     url = f'{base_url}&start={page * 25}'
@@ -49,72 +52,54 @@ for page in range(num_pages):
 
     # Extract information from each job posting
     for job in job_postings:
-        try:
-            # Extract job title
-            title_elem = job.find_element(By.CSS_SELECTOR, 'h3.base-search-card__title')
-            title = title_elem.text.strip() if title_elem else 'N/A'
+        # Extract job title
+        title = WebScraperHelper.safe_find_text(job, By.CSS_SELECTOR, 'h3.base-search-card__title')
 
-            # Extract company name    
-            company_elem = job.find_element(By.CSS_SELECTOR, 'h4.base-search-card__subtitle')
-            company = company_elem.text.strip() if company_elem else 'N/A'
+        # Extract company name
+        company = WebScraperHelper.safe_find_text(job, By.CSS_SELECTOR, 'h4.base-search-card__subtitle')
 
-            # Extract job location
-            location_elem = job.find_element(By.CSS_SELECTOR, 'span.job-search-card__location')
-            location = location_elem.text.strip() if location_elem else 'N/A'
+        # Extract job location
+        location = WebScraperHelper.safe_find_text(job, By.CSS_SELECTOR, 'span.job-search-card__location')
 
-            # Extract job link
-            joblink_elem = job.find_element(By.CSS_SELECTOR, 'a.base-card__full-link')
-            joblink = joblink_elem.get_attribute('href').strip() if joblink_elem else 'N/A'
+        # Extract job link
+        joblink = WebScraperHelper.safe_find_link(job, By.CSS_SELECTOR, 'a.base-card__full-link')
 
-            # Click on the job link to get detailed information
-            joblink_elem.click()
-            SleepHelper.random_sleep(15, 3)  # wait for the job detail page to load
+        # Click on the job link to get detailed information
+        WebScraperHelper.safe_find_click(job, By.CSS_SELECTOR, 'a.base-card__full-link')
+
+        SleepHelper.random_sleep(15, 3)
 
 
-            # Extract job description
-            description_elem = driver.find_element(By.CSS_SELECTOR, 'div.description__text')
-            description = description_elem.text.strip() if description_elem else 'N/A'
+        # Extract job description
+        description = WebScraperHelper.safe_find_text(driver, By.CSS_SELECTOR, 'div.description__text')
 
-            # Extract job details (e.g., salary, employment type, etc.)
-            detail_sections = driver.find_elements(By.CSS_SELECTOR, 'li.description__job-criteria-item')
-            details = {}
-            
-            key_elem = driver.find_element(By.CSS_SELECTOR, 'div.compensation__salary-range').find_element(By.CSS_SELECTOR, 'h3.compensation__heading')
-            value_elem = driver.find_element(By.CSS_SELECTOR, 'div.compensation__salary-range').find_element(By.CSS_SELECTOR, 'div.compensation__salary')
-            key = key_elem.text.strip() if key_elem else 'N/A'
-            value = value_elem.text.strip() if value_elem else 'N/A'
+        # Extract job details (e.g., salary, employment type, etc.)
+        details = {}
+
+        key = WebScraperHelper.safe_find_text_by_selectors(driver,[(By.CSS_SELECTOR, 'div.compensation__salary-range'),(By.CSS_SELECTOR, 'h3.compensation__heading')])
+        value = WebScraperHelper.safe_find_text_by_selectors(driver, [(By.CSS_SELECTOR, 'div.compensation__salary-range'),(By.CSS_SELECTOR, 'div.compensation__salary')])
+        details[key] = value
+
+        detail_sections = driver.find_elements(By.CSS_SELECTOR, 'li.description__job-criteria-item')
+
+        for section in detail_sections:
+            key = WebScraperHelper.safe_find_text(section, By.CSS_SELECTOR, 'h3.description__job-criteria-subheader')
+            value = WebScraperHelper.safe_find_text(section, By.CSS_SELECTOR, 'span.description__job-criteria-text')
             details[key] = value
-            
-            for section in detail_sections:
-                key_elem = section.find_element(By.CSS_SELECTOR, 'h3.description__job-criteria-subheader')
-                value_elem = section.find_element(By.CSS_SELECTOR, 'span.description__job-criteria-text')
-                key = key_elem.text.strip() if key_elem else 'N/A'
-                value = value_elem.text.strip() if value_elem else 'N/A'
-                details[key] = value
 
-            # Create a dictionary to store the job data
-            job_data = {
-                'source': 'linkedin',
-                'title': title,
-                'company': company,
-                'location': location,
-                'job_link': joblink,
-                'details': details,
-                'description': description
-            }
-            
-            # print(json.dumps(job_data, indent=4))
-            # print('-' * 40)
-            
-            # Insert the job data into MongoDB
-            collection.insert_one(job_data)
+        # Create a dictionary to store the job data
+        job_data = {
+            'source': 'linkedin',
+            'title': title,
+            'company': company,
+            'location': location,
+            'job_link': joblink,
+            'details': details,
+            'description': description
+        }
 
-            # # Close the job detail window and switch back to the main window
-            # driver.close()
-            # driver.switch_to.window(driver.window_handles[0])
+        # Insert the job data into MongoDB
+        collection.insert_one(job_data)
 
-        except Exception as e:
-            print(f"Error processing job: {e}")
-            continue
 
 driver.quit()
